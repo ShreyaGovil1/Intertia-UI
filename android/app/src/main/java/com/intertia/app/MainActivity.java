a09a1fbee92176cdab1a511c5523cd0cca6db3dc
shreyagovil <shreyagovil07@gmail.com>
2025-12-27 21:49:17 +0530

    Added IOS and Andriod Wrapper to run on playstore and appstore

---
+++ android/app/src/main/java/com/intertia/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.intertia.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.intertia.app',
+  appName: 'Inertia',
+  webDir: 'dist'
+};
+
+export default config;
